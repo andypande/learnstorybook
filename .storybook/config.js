1790cd3cb65eabf42f1bd4a922ca0b3dde7ce696
Andy Pande <andypande@gmail.com>
2019-11-19 15:42:16 -0600

    Documentation add on and MDX integration!
    Integrate Documentation into storybook based on this course
    https://www.learnstorybook.com/design-systems-for-developers/react/en/document/

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import 'storybook-chromatic';
 import { GlobalStyle } from '../src/shared/global';
 import React from 'react';
 
@@ -11,4 +13,10 @@ addDecorator(story => (
 
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+configure(
+  [
+    require.context('../src', false, /Intro\.stories\.mdx/),
+    require.context('../src', true, /\.stories\.(js|mdx)$/)
+  ],
+  module
+);
